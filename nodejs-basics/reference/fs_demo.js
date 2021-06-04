a3770f04b85dd8227eda5e3d02d93f34d9d072be
Khurshidbek <kabilovhurshid5@gmail.com>
2021-06-04 19:53:59 +0500

    node fs module

---
+++ nodejs-basics/reference/fs_demo.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+// Create folder
+// fs.mkdir(path.join(__dirname, 'test'), {}, (err) => {
+//     if (err) throw err;
+//     console.log('Folder created!');
+// });
+
+// Create and write to file
+// fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), 'Hello from fs system!', (err) => {
+//     if (err) throw err;
+//     console.log('File written to!');
+// });
+
+// fs.appendFile(path.join(__dirname, 'test', 'hello.txt'), ' I love Node.js', (err) => {
+//     if (err) throw err;
+//     console.log('File written to!');
+// });
+
+// Read file
+// fs.readFile(path.join(__dirname, 'test', 'hello.txt'), 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+// Rename file
+fs.rename(path.join(__dirname, 'test', 'hello.txt'), path.join(__dirname, 'test', 'hi.txt'), (err, data) => {
+    if (err) throw err;
+    console.log('File renamed!');
+});
