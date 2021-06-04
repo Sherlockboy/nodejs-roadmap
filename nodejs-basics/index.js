b1333c9c2cb0d021cb3b7dd82bb79841a269967b
Khurshidbek <kabilovhurshid5@gmail.com>
2021-06-04 11:42:53 +0500

    exporting class example

--- nodejs-basics/index.js
+++ nodejs-basics/index.js
@@ -1,3 +1,5 @@
-const person = require('./person');
+const Person = require('./person');
 
-console.log(person);
+const person1 = new Person('John Doe', 30);
+
+person1.greeting();
--- nodejs-basics/person.js
+++ nodejs-basics/person.js
@@ -1,6 +1,12 @@
-const person = {
-    name: 'John Doe',
-    age: 30
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`Hi! My name is ${this.name} and I am ${this.age}`);
+    }
 }
 
-module.exports = person;
+module.exports = Person;
