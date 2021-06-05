6e9b60d2bade32a05bc6f54cbb8cb0766ec64506
Khurshidbek <kabilovhurshid5@gmail.com>
2021-06-06 00:37:47 +0500

    simple node server example

--- nodejs-basics/index.js
+++ nodejs-basics/index.js
@@ -3,45 +3,56 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        fs.readFile(
-            path.join(__dirname, 'public', 'index.html'),
-            'utf8',
-            (err, data) => {
-                if (err) throw err;
-                res.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                res.end(data);
+    // File path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+    // File extension
+    let extname = path.extname(filePath);
+
+    // Content type
+    let contentType = 'text/html';
+
+    // Check extension and set content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, data) => {
+                    res.writeHead(404, {
+                        'Content-Type': 'text/html'
+                    })
+                    res.end(data, 'utf8')
+                })
+            } else {
+                // Server error
+                res.writeHead(500)
+                res.end(`Server error: ${err.code}`)
             }
-        );
-    }
-
-    if (req.url === '/about') {
-        fs.readFile(
-            path.join(__dirname, 'public', 'about.html'),
-            'utf8',
-            (err, data) => {
-                if (err) throw err;
-                res.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                res.end(data);
-            }
-        );
-    }
-
-    if (req.url === '/api/users') {
-        const users = [
-            { name: 'John',age: '20' },
-            { name: 'Barry', age: '22' }
-        ];
-
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-        res.end(JSON.stringify(users));
-    }
+        } else {
+            // Success
+            res.writeHead(200, {
+                'Content-Type': contentType
+            })
+            res.end(data, 'utf8')
+        }
+    })
 })
 
 const PORT = process.env.PORT || 5000
