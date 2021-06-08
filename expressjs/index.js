ec30950fb767c1bc97458d06e7ff1e10801b6d51
Khurshidbek <kabilovhurshid5@gmail.com>
2021-06-08 11:23:04 +0500

    returning file

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
