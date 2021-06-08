32683511238d07bc67d68279b4c4a3ba26b7c1ae
Khurshidbek <kabilovhurshid5@gmail.com>
2021-06-08 16:08:10 +0500

    api for single member

--- expressjs/index.js
+++ expressjs/index.js
@@ -13,6 +13,16 @@ const PORT = process.env.PORT || 5000;
 // Get all members
 app.get('/api/members', (req, res) => res.json(members));
 
+// Get single user
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+    if(found){
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(404).json({message: `Member with id ${req.params.id} not found!`});
+    }
+});
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
