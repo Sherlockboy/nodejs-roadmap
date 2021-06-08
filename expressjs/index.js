3029e3fbd3d8415be48ff2947912c4cec0af25b6
Khurshidbek <kabilovhurshid5@gmail.com>
2021-06-08 11:45:42 +0500

    middleware example

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,36 +1,18 @@
 const express = require('express');
 const path = require('path');
+const members = require('./members');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const members = [
-    {
-        id: 1,
-        name: 'John',
-        email: 'john@gmail.com',
-        status: 'active'
-    },
-    {
-        id: 2,
-        name: 'Jack',
-        email: 'jack@gmail.com',
-        status: 'inactive'
-    },
-    {
-        id: 3,
-        name: 'Max',
-        email: 'max@gmail.com',
-        status: 'active'
-    },
-    {
-        id: 4,
-        name: 'Barry',
-        email: 'barry@gmail.com',
-        status: 'inactive'
-    }
-];
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
+// Init middleware
+app.use(logger);
 
 // Get all members
 app.get('/api/members', (req, res) => res.json(members));
---
+++ expressjs/members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id: 1,
+        name: 'John',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jack',
+        email: 'jack@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Max',
+        email: 'max@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 4,
+        name: 'Barry',
+        email: 'barry@gmail.com',
+        status: 'inactive'
+    }
+];
+
+module.exports = members;
