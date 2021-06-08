8e2f9535b7cc876361f99a30fb3673f287ddde5e
Khurshidbek <kabilovhurshid5@gmail.com>
2021-06-08 11:35:16 +0500

    simple rest api example

--- expressjs/index.js
+++ expressjs/index.js
@@ -5,6 +5,36 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const members = [
+    {
+        id: 1,
+        name: 'John',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jack',
+        email: 'jack@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Max',
+        email: 'max@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 4,
+        name: 'Barry',
+        email: 'barry@gmail.com',
+        status: 'inactive'
+    }
+];
+
+// Get all members
+app.get('/api/members', (req, res) => res.json(members));
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
