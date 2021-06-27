1e2382b5a857fbbdfe2f2cacdc2d74de63ab9dc2
Khurshidbek <kabilovhurshid5@gmail.com>
2021-06-27 13:53:46 +0500

    save post with async

--- men-rest-api/routes/posts.js
+++ men-rest-api/routes/posts.js
@@ -6,21 +6,20 @@ router.get('/', (req, res) => {
     res.send('We are on posts');
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
 
-    post.save()
-        .then(data => {
-            res.status(200).json(data);
-        })
-        .catch(err => {
-            res.status(500).json({
-                message: err
-            });
-        })
+    try {
+        const savedPost = await post.save();
+        res.status(200).json(savedPost);
+    } catch (err) {
+        res.status(500).json({
+            message: err
+        });
+    }
 });
 
 module.exports = router;
