04d260bebdcdcd7edc3dd1811850d7a4f940c33a
Khurshidbek <kabilovhurshid5@gmail.com>
2021-06-30 11:38:59 +0500

    CRUD finished

--- men-rest-api/routes/posts.js
+++ men-rest-api/routes/posts.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('We are on posts');
+// Get all posts
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json({
+            message: err
+        });
+    }
 });
 
+// Create post
 router.post('/', async (req, res) => {
     console.log(req.body);
     const post = new Post({
@@ -16,6 +25,50 @@ router.post('/', async (req, res) => {
     try {
         const savedPost = await post.save();
         res.status(200).json(savedPost);
+    } catch (err) {
+        res.status(500).json({
+            message: err
+        });
+    }
+});
+
+// Get single post
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json({
+            message: err
+        });
+    }
+});
+
+// Delete single post
+router.delete('/:id', async (req, res) => {
+    try {
+        const deleteStatus = await Post.remove({
+            _id: req.params.id
+        });
+        res.status(200).json(deleteStatus);
+    } catch (err) {
+        res.status(500).json({
+            message: err
+        });
+    }
+});
+
+// Update post
+router.patch('/:id', async (req, res) => {
+    try {
+        const updateStatus = await Post.updateOne({
+            _id: req.params.id
+        }, {
+            $set: {
+                title: req.body.title
+            }
+        });
+        res.status(200).json(updateStatus);
     } catch (err) {
         res.status(500).json({
             message: err
